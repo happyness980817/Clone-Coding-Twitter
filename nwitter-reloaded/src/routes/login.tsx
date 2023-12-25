b65ca7ba17772574f4b02480a9666793851e7cea
happyness980817 <happyness980817@gmail.com>
2023-12-25 17:13:58 +0900

    Log In is fucking easy

--- nwitter-reloaded/src/routes/login.tsx
+++ nwitter-reloaded/src/routes/login.tsx
@@ -1,3 +1,83 @@
-export default function Login() {
-    return <h1>login</h1>;
-  }
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  Form,
+  Error,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from "../components/auth-components"
+
+export default function CreateAccount() {
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(""); // initialize error message
+    if (isLoading || email === "" || password === "") return; // terminate function
+    try {
+      // create an account
+      // set the name of the user.
+      // redirect to the home page
+      setLoading(true); // Preventing Multiple Submissions
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/"); // send logged in user to home
+    } catch (e) {
+      // setError
+      if(e instanceof FirebaseError){
+        setError(e.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <Wrapper>
+      <Title>Log into 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          onChange={onChange}
+          value={password}
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Log In"}
+        />
+      </Form>
+      <Switcher>
+        Don't have an account? <Link to="/create-account">Create One &rarr;</Link>
+      </Switcher>
+      {error !== "" ? <Error>{error}</Error> : null}
+    </Wrapper>
+  );
+}
