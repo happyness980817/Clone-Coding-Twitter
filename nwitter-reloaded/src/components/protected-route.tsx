1270c7d6f03c4faba9da2a73cb6c3e03ed164164
happyness980817 <happyness980817@gmail.com>
2023-12-25 16:02:45 +0900

    logout if fucking easy. awesome.

--- nwitter-reloaded/src/components/protected-route.tsx
+++ nwitter-reloaded/src/components/protected-route.tsx
@@ -3,8 +3,8 @@ import { auth } from "../firebase";
 
 export default function ProtectedRoute({children,}: {children: React.ReactNode;}){
   const user = auth.currentUser; // user 에게 정보 요청
-  if(!user){
+  if(user === null){
     return <Navigate to="/login" /> // moves user to "login"
   }
-  return children
+  return children // this becomes <Home /> or <Profile />
 }
