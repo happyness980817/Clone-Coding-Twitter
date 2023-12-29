5c9b6636fb1d5ee7f62963165e4dc7842ac8d0fa
happyness980817 <happyness980817@gmail.com>
2023-12-29 14:51:37 +0900

    tweeting to firestore

--- nwitter-reloaded/src/components/post-tweet-form.tsx
+++ nwitter-reloaded/src/components/post-tweet-form.tsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from "firebase/firestore";
 import { useState } from "react";
 import { styled } from "styled-components"
+import { auth, db } from "../firebase";
 
 const Form = styled.form`
     display: flex;
@@ -70,7 +72,26 @@ export default function PostTweetForm(){
             setFile(files[0]); // 하나의 파일만 업로드 가능
         }
     };
-    return (<Form>
+    const onSubmit = async(e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const user = auth.currentUser
+        if(!user || isLoading || tweet === "" || tweet.length > 180){return};
+
+        try {
+            setLoading(true);
+            await addDoc(collection(db,"tweets"),{
+                tweet,
+                createdAt: Date.now(),
+                username: user.displayName || "Anonymous",
+                userId: user.uid,
+            })
+        } catch(e) {
+            console.log(e);
+        } finally {
+            setLoading(false);
+        }
+    };
+    return (<Form onSubmit={onSubmit}>
         <TextArea rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?" />
         <AttachFileButton htmlFor="file">{file ? "Photo added ✅" : "Add photo"}</AttachFileButton>
         <AttachFileInput onChange={onFileChange} type="file" id="file" accept="image/*" /> 
